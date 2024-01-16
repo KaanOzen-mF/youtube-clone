fd49b51425df8820c2d5812fe6f541b51407d403
KaanOzen-mF <kaanozn98@gmail.com>
2024-01-16 15:32:52 +0300

    Videos added

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Stack, Box } from "@mui/material";
+import { ChannelCard, VideoCard } from "./";
 
-function Videos() {
-  return <div>Videos</div>;
+function Videos({ videos }) {
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.items &&
+        videos.items.map((video, idx) => (
+          <Box key={idx}>
+            {video.id.videoId && <VideoCard video={video} />}
+            {video.snippet.channelId && <ChannelCard channelDetail={video} />}
+          </Box>
+        ))}
+    </Stack>
+  );
 }
 
 export default Videos;
