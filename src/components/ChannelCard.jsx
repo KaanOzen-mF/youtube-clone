1b642129b166c82505b8157d17dd08e32968f092
KaanOzen-mF <kaanozn98@gmail.com>
2024-01-16 15:33:29 +0300

    Channel Card created

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,11 @@
+import { Typography, Box, CardContent, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+
+import { demoProfilePicture } from "../utils/constants";
+
+function ChannelCard({ channelDetail }) {
+  return <Box sx={{ boxShadow: "none", borderRadius: "20px" }}></Box>;
+}
+
+export default ChannelCard;
