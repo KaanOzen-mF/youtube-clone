759874833d810d2eed65b59066e0aed39e9e171b
KaanOzen-mF <kaanozn98@gmail.com>
2024-01-16 15:28:38 +0300

    VideoCard and ChannelCard export

--- src/components/index.js
+++ src/components/index.js
@@ -7,3 +7,5 @@ export { default as SearchFeed } from "./SearchFeed";
 export { default as SideBar } from "./Sidebar";
 export { default as SearchBar } from "./SearchBar";
 export { default as Videos } from "./Videos";
+export { default as VideoCard } from "./VideoCard";
+export { default as ChannelCard } from "./ChannelCard";
