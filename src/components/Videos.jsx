a62683a95f1c4e49ea7821dd768e2fab8bd2d4ed
KaanOzen-mF <kaanozn98@gmail.com>
2024-01-18 17:18:32 +0300

    videos jsx element change videos.item change to videos

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { Stack, Box } from "@mui/material";
-import { ChannelCard, VideoCard } from "./";
+import { VideoCard } from "./";
 
 function Videos({ videos }) {
   return (
     <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
-      {videos.items &&
-        videos.items.map((video, idx) => (
-          <Box key={idx}>
-            {video.id.videoId && <VideoCard video={video} />}
-            {video.snippet.channelId && <ChannelCard channelDetail={video} />}
-          </Box>
+      {videos &&
+        videos.map((video, idx) => (
+          <Box key={idx}>{video.id.videoId && <VideoCard video={video} />}</Box>
         ))}
     </Stack>
   );
