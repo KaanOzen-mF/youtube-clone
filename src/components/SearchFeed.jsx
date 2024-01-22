ae7c5764db9cfb3b748326ce9f4a44e84cb0001a
KaanOzen-mF <kaanozn98@gmail.com>
2024-01-22 11:47:18 +0300

    Search Feed works responsivelly

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -15,7 +15,11 @@ function SearchFeed() {
   }, [searchTerm]);
 
   return (
-    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+    <Box
+      p={2}
+      sx={{ overflowY: "auto", height: "90vh", flex: 2 }}
+      ml={{ xs: "0", md: "16" }}
+    >
       <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
         Search Results for:
         <span style={{ color: "#F31503", marginLeft: "8px" }}>
